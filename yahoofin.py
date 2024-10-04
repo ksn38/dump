6609872e4fc53c70580225dbc0b311ddac70e4d4
ksn38 <ksn38@yandex.ru>
2024-10-04 13:04:31 +0300

    -

--- yahoofin.py
+++ yahoofin.py
@@ -1,33 +1,53 @@
 import os
 from datetime import date
 from datetime import timedelta
-from collections import OrderedDict
 from bs4 import BeautifulSoup as bs
 import requests
-import time
+import re
 
-def ticks(*args):
+urli = 'https://finance.yahoo.com/markets/world-indices/'
+urlt = 'https://finance.yahoo.com/markets/bonds/'
+regi = '\" data-field=\"regularMarketPrice\" data-trend=\"none\" data-pricehint=\"2\" data-value=\"\d*\.\d*\" active'
+regt = '\" data-field=\"regularMarketChangePercent\" data-trend=\"txt\" data-pricehint=\"4\" data-value=\"\d*\.\d*\" data-tstyle=\"default\"'
+        
+def ticks(url, reg, *args):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
-    t_dict = OrderedDict()
+    
+    d = dict()
+    response = requests.get(url, headers=headers).text
 
     for i in (args):
-        if i not in {'wti', 'gold', 'sz', 'wheat', 'ss', 'cop'}:
-            url = 'https://finance.yahoo.com/quote/^' + i + '/history'
+        x = re.findall(i + reg, response)
+        if i != '000001.SS':
+            d[str.lower(i)] = float(re.findall('\d+\.\d+', str(x))[0])
         else:
-            commodities = {'wti': 'CL=F', 'gold': 'GC=F', 'wheat': 'KE=F', 'sz': '399001.SZ', 'ss': '000001.SS', 'cop': 'HG=F'}
-            url = 'https://finance.yahoo.com/quote/' + commodities[i] + '/history'
-            
-        response = requests.get(url, headers=headers).text
-        parsed_html = bs(response, 'lxml')
-        #print(parsed_html)
-        t = parsed_html.find('fin-streamer', {'class': 'livePrice yf-1i5aalm'}).text.replace(',', '')
-        print(t)
-        f = open("gspc.txt", "w", encoding="utf-8")
-        f.write(response)
-        f.close()
-        #t_dict[i] = float(t)
+            d['ss'] = float(re.findall('\d+\.\d+', str(x))[0])
 
-    return t_dict
+    return d
+
+def trec():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
+    d = dict()
     
-print(ticks('wti'))
+    response = requests.get('https://tradingeconomics.com/commodities', headers=headers).text
+    parsed_html = bs(response, 'lxml')
+    
+    for i in ('CL1:COM', 'W 1:COM', 'HG1:COM', 'XAUUSD:CUR'):
+        t = parsed_html.find('tr', {'data-symbol': i}).text
+        t = ' '.join(t.split())
+        name = re.findall('^\w+', t)
+        val = re.findall('\d+\.\d+', t)[0]
+        if name[0] != 'Crude':
+            d[str.lower(name[0])] = float(val)
+        else:
+            d['wti'] = float(val)
+
+    return d
+    
+t_dict = dict()    
+t_dict.update(ticks(urli, regi, 'GSPC', 'IXIC', 'RUT', 'VIX', 'GDAXI', 'BVSP', '000001.SS', 'BSESN'))
+t_dict.update(ticks(urlt, regt, 'TNX'))
+t_dict.update(trec())
+print(t_dict)
