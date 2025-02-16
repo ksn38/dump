05dad7b8bd2406b0c05621827d8ac279898850c8
ksn38 <ksn38@yandex.ru>
2025-02-16 21:47:28 +0300

    -

---
+++ extrusionDepth.js
@@ -0,0 +1,29 @@
+(function renameLayerSource() {
+    app.beginUndoGroup("Rename Layer Source");
+    var myComp = app.project.activeItem;
+    var layers = ["Afghanistan Outlines", "Armenia Outlines", 
+    "Azerbaijan Outlines", "Bangladesh Outlines", "Bhutan Outlines", 
+    "Brunei Darussalam Outlines", "Cambodia Outlines", "China Outlines", 
+    "Cyprus Outlines", "Egypt, Arab Rep Outlines", "Georgia Outlines", 
+    "India Outlines", "Indonesia Outlines", "Iran, Islamic Rep Outlines", 
+    "Iraq Outlines", "Israel Outlines", "Japan Outlines", "Jordan Outlines", 
+    "Kazakhstan Outlines", "Kuwait Outlines", "Kyrgyz Republic Outlines", 
+    "Lao PDR Outlines", "Lebanon Outlines", "Malaysia Outlines", "Mongolia Outlines", 
+    "Myanmar Outlines", "Nepal Outlines", "Korea, Dem Outlines", 
+    "Oman Outlines", "Pakistan Outlines", "Philippines Outlines", "Russian Federation Outlines", 
+    "Qatar Outlines", "Saudi Arabia Outlines", "Singapore Outlines", 
+    "Korea, Rep Outlines", "Sri Lanka Outlines", "Syrian Arab Republic Outlines", 
+    "Tajikistan Outlines", "Thailand Outlines", "Timor-Leste Outlines", 
+    "Turkmenistan Outlines", "Turkiye Outlines", "United Arab Emirates Outlines", 
+    "Uzbekistan Outlines", "Viet Nam Outlines", "Yemen, Rep Outlines"];
+        
+    for (var i = 0; i < layers.length; i++){
+        //var newName = prompt("print Outlines", layers[i]);
+        var chkLayer = myComp.layer(layers[i]);
+        chkLayer.geometryOption.extrusionDepth.expression = ('x = thisComp.layer("asia.mgjson")("Data")("Outline")("' + layers[i].slice(0,-9) + '");\
+slider = (thisComp.layer("slider height").transform.position[0])/10;\
+x*slider');
+    }
+    //var newName = prompt("Rename Layer Source");
+    app.endUndoGroup();
+})();
